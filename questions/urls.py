823cd6099b3c985f3aaffa116aa1fc3b9ba017ac
shen.xianpeng <shenxianpeng1991@gmail.com>
2013-12-20 22:26:11 +0800

    add urlconfig for new answer view

--- questions/urls.py
+++ questions/urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 
-from questions.views import QuestionDetail, NewQuestionAjaxView
+from questions.views import QuestionDetail, NewQuestionAjaxView, NewAnswerAjaxView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -10,5 +10,6 @@ from questions.views import QuestionDetail, NewQuestionAjaxView
 urlpatterns = patterns('',
     url(r'^(?P<id>\d+)/$', QuestionDetail(), name='questions-question_detail'),
     url(r'^new/$', NewQuestionAjaxView(), name='questions-new_question_form'),
+    url(r'^(?P<id>\d+)/answer/new/$', NewAnswerAjaxView(), name='questions-new_answer'),
 )
 
