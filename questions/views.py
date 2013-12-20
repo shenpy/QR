f2dc95d1916f94e686d18abe64216a3fa5abf4fa
shen.xianpeng <shenxianpeng1991@gmail.com>
2013-12-20 22:23:41 +0800

    create NewAnswerView, add argumment for view

--- questions/views.py
+++ questions/views.py
@@ -7,8 +7,7 @@ from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 
 from questions.models import *
-from questions.forms import QuestionForm
-
+from questions.forms import QuestionForm, AnswerForm
 
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, \
@@ -27,7 +26,7 @@ class MyBaseView:
         """
 
         wraps(function)
-        def wrap(self, request):
+        def wrap(self, request, *args, **kwargs):
             permission = (
                     request.is_ajax(),
                     request.user.is_authenticated()
@@ -35,12 +34,12 @@ class MyBaseView:
             if not all(permission):
                 return HttpResponseNotAllowed('AJAX')
             self.request=request
-            return function(self, request)
+            return function(self, request, *args, **kwargs)
         #wrap.__doc__ = function.__doc__
         #wrap.__name__ = function.__name__
         return wrap
 
-    def __call__(self, request):
+    def __call__(self, request, *args, **kwargs):
         if not request.user.is_authenticated():
             return HttpResponseForbidden()
         self.request=request
@@ -50,7 +49,7 @@ class MyBaseView:
             allowed_methods = [m.lstrip("do_") for m in dir(self)
                                                 if m.startswith("do_")]
             return HttpResponseNotAllowed(allowed_methods)
-        return callback()
+        return callback(*args, **kwargs)
 
 
 class QuestionDetail:
@@ -59,21 +58,23 @@ class QuestionDetail:
         question = get_object_or_404(Question, id=id)
         #json = serializers.serialize("json", [question])
         #return HttpResponse(json, mimetype="application/json")
-        context = RequestContext(request, {'question': question})
+        answerform = AnswerForm()
+        context = RequestContext(request, {'question': question,
+                                           'answerform': answerform})
         template  = loader.get_template('questions/question_detail.html')
         return HttpResponse(template.render(context))
 
 
 class NewQuestionAjaxView(MyBaseView):
 
-    def do_GET(self):
+    def do_GET(self, *args, **kwargs):
         form = QuestionForm()
         form.label_suffix=''
         context = RequestContext(self.request, {'form': form})
         template  = loader.get_template('questions/ajax_new_question_form.html')
         return HttpResponse(template.render(context))
 
-    def do_POST(self):
+    def do_POST(self, *args, **kwargs):
         form = QuestionForm(self.request.POST)
         if form.is_valid():
             question = Question(title=form.cleaned_data['title'],
@@ -84,3 +85,19 @@ class NewQuestionAjaxView(MyBaseView):
         return HttpResponseRedirect('/')
 
 
+class NewAnswerAjaxView(MyBaseView):
+
+    def do_POST(self, *args, **kwargs):
+        if self.request.user.is_authenticated():
+            answerform = AnswerForm(self.request.POST)
+    # is     there better way to save answer with its question so i don'r need to get question
+    # yep  set foreignkey using integer  '''field_id = kwargs['id']'''
+            if answerform.is_valid():
+                answer = Answer(text=answerform.cleaned_data['text'])
+                answer.replyer = self.request.user
+                answer.question_id = kwargs['id']
+                answer.save()
+                json = serializers.serialize("json", [answer])
+                return HttpResponse(json, mimetype="application/json")
+        else:
+            return HttpResponseForbidden()
