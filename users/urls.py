ddbb32592a78d81eb740d7eff6c5ac863113a946
shen.xianpeng <shenxianpeng1991@gmail.com>
2014-01-03 09:31:25 +0800

    add urlpattern for user relationship

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@
 from django.conf.urls import patterns, include, url
 
 from users.views import UserHomeView, LoginView, LogoutView, SignupView, \
-                            UserView
+                            UserView, follow, unfollow
 
 
 urlpatterns = patterns('',
@@ -11,4 +11,6 @@ urlpatterns = patterns('',
     url(r'^login/$', LoginView.as_view(), name='users-login'),
     url(r'^logout/$', LogoutView.as_view(), name='users-logout'),
     url(r'^(?P<id>\d+)/$', UserView(), name='users-user'),
+    url(r'^(?P<id>\d+)/follow/$', follow, name='users-follow'),
+    url(r'^(?P<id>\d+)/unfollow/$', unfollow, name='users-unfollow'),
 )
