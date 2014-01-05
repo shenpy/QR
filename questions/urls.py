7efa7b97cf409c3658df084ab43ce125e21dcdb4
shen.xianpeng <shenxianpeng1991@gmail.com>
2014-01-05 15:45:27 +0800

    add answer vote urlpattern

--- questions/urls.py
+++ questions/urls.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 
-from questions.views import QuestionDetail, NewQuestionAjaxView, NewAnswerAjaxView
+from questions.views import QuestionDetail, \
+                            NewQuestionAjaxView, NewAnswerAjaxView, \
+                            vote, unvote
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^(?P<id>\d+)/$', QuestionDetail(), name='questions-question_detail'),
     url(r'^new/$', NewQuestionAjaxView(), name='questions-new_question_form'),
     url(r'^(?P<id>\d+)/answer/new/$', NewAnswerAjaxView(), name='questions-new_answer'),
+    url(r'^answer/(?P<id>\d+)/vote/$', vote, name='questions-answer_vote'),
+    url(r'^answer/(?P<id>\d+)/unvote/$', unvote, name='questions-answer_unvote'),
 )
-
