1faabaafce41a88a4688fa085e98df9c15c18808
shen.xianpeng <shenxianpeng1991@gmail.com>
2014-01-16 18:16:11 +0800

    runserver --nostatic: let nginx serve static file

--- design/urls.py
+++ design/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from .views import IndexView
 
 # Uncomment the next two lines to enable the admin:
@@ -10,3 +12,6 @@ urlpatterns = patterns('',
     url(r'^user/', include('users.urls')),
     url(r'^question/', include('questions.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
