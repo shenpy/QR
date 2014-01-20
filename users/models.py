2b585e428fad2665e80a651ead9d3709b6a82008
shen.xianpeng <shenxianpeng1991@gmail.com>
2014-01-20 22:02:29 +0800

    users relationship extending

--- users/models.py
+++ users/models.py
@@ -30,8 +30,10 @@ class User(AbstractBaseUser):
     username = models.CharField(max_length=40, unique=True,
                                         db_index=True)
     email = models.EmailField(max_length=254, unique=True)
-    following = models.ManyToManyField('self', symmetrical=False, related_name='followers', db_table='user_relationship')
-
+    relating = models.ManyToManyField('self',
+                                      symmetrical=False,
+                                      through='Relationship',
+                                      related_name='related_to')
 
     objects = MyUserManager()
 
@@ -56,3 +58,56 @@ class User(AbstractBaseUser):
                 return True
         else:
             return False
+
+    @property
+    def followings(self):
+        return self.get_relating(RELATIONSHIP_FOLLOWING)
+
+    @property
+    def followers(self):
+        return self.get_related(RELATIONSHIP_FOLLOWING)
+
+    def add_following(self, user):
+        self.add_relationship(user, RELATIONSHIP_FOLLOWING)
+
+    def remove_following(self, user):
+        self.remove_relationship(user, RELATIONSHIP_FOLLOWING)
+
+    def add_relationship(self, user, status):
+        relationship, created = Relationship.objects.get_or_create(
+            from_user=self,
+            to_user=user,
+            status=status)
+        return relationship
+
+    def remove_relationship(self, user, status):
+        Relationship.objects.filter(
+            from_user=self,
+            to_user=user,
+            status=status).delete()
+        return
+
+    def get_relating(self, status):
+        return self.relating.filter(
+            to_user__status=status,
+            to_user__from_user=self)
+
+    def get_related(self, status):
+        return self.related_to.filter(
+            from_user__status=status,
+            from_user__to_user=self)
+
+
+RELATIONSHIP_FOLLOWING = 1
+RELATIONSHIP_BLOCKED = 2
+RELATIONSHIP_STATUSES = (
+    (RELATIONSHIP_FOLLOWING, 'Following'),
+    (RELATIONSHIP_BLOCKED, 'Blocked'),
+)
+
+class Relationship(models.Model):
+    from_user = models.ForeignKey(User, related_name='from_user')
+    to_user = models.ForeignKey(User, related_name='to_user')
+    status = models.IntegerField(choices=RELATIONSHIP_STATUSES)
+
+
